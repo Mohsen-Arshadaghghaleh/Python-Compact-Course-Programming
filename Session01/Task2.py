6cf4c8e7dbd30933a3d049124608a5c61301c3bb
Mohsen Arshad <mohsen.arshadaghghaleh@gmail.com>
2026-09-21 20:15:33 +0330

    Task2
    feat: add Task 2 - variable declaration and output

---
+++ Session01/Task2.py
@@ -0,0 +1,17 @@
+# Declare and initialize
+zahl = 10                   # integer
+kommazahl = 10.5            # float
+text = "Hello, World!"      # string
+wahrheitswert = True        # boolean
+
+# Output
+print("zahl(Integer): ", zahl)
+print("kommazahl(Float): ", kommazahl)
+print("text(String): ", text)
+print("wahrheitswert(Boolean): ", wahrheitswert)
+
+# Check the type
+print("Type of zahl: ", type(zahl))
+print("Type of kommazahl: ", type(kommazahl))
+print("Type of text: ", type(text))
+print("Type of wahrheitswert: ", type(wahrheitswert))
